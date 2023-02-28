7a2e6e517e213da3d2d1b1f14114774160eee349
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-02-28 01:32:06 -0900

    Update MicroSolar.py

--- Solar/MicroSolar.py
+++ Solar/MicroSolar.py
@@ -72,7 +72,8 @@ def solarPanel():
     print("Anchorage Alaska receives approximately 5.06 kWh of sunlight per square meter per day in June")
     print ("Anchorage Alaska receives approximately 19.2 hours of sunlight per day in June\n")    
     
-def main():    
+def main():
+    """This function calls the various script functions"""
     hoursCalc()
     microPower()
     solarPanel()
