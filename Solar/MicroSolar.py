d4dd5f0091c9a3ff7b27dc9f4778e9227b0adfc9
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-02-28 01:24:09 -0900

    Update MicroSolar.py

--- Solar/MicroSolar.py
+++ Solar/MicroSolar.py
@@ -1,4 +1,4 @@
-# This script will provide a rough estimate for a solar panel and battery for a microcontroller
+# This script will provide a calculation that sizes a solar panel and battery for a microcontroller
 
 # References 
     # Andreas Spiess
@@ -7,19 +7,29 @@
     
     # NREL Photovoltatic Calculator
     # https://pvwatts.nrel.gov/index.php
+    
+    # Alaska dot org Daylight Hours Calculator
+    # https://www.alaska.org/weather/daylight-hours/anchorage/december
+    
+    # NWS Weather Data for Anchorage Alaska
+    # https://w1.weather.gov/data/obhistory/PANC.html
+    
+# Made with Mu v.1.0.3
+    # February 2023
+    # https://codewith.mu
 
 hours = 0
 watts = 0
 
-def yearlyHours():
-    """This function calculates the number of hours in a year and updates a global variable"""
+def Hours():
+    """This function calculates the number of hours and updates a global variable"""
     #print("There are 365 days in a year")
     #print("There are 24 hours in a day")
     #print("There are 8760 hours in a year")
-    yH =+ 365*24
-    print("There are ", yH, " hours in a year\n")
+    yH = 365*24
+    #print("There are ", yH, " hours in a year\n")
 
-    # this updates a global variable and is bad form ... todo redo script using object oriented programming
+    # this updates a global variable and is bad form ... to do: redo script using object oriented programming
     global hours
     #print(hours)  
     hours = yH + hours
@@ -42,19 +52,28 @@ def microPower():
     #print("A typical IoT microcontroller requires 0.5 Watts per hour or 4.3 kWh per year\n")    
     print("A typical IoT microcontroller requires 0.5 Watts per hour or", hours*0.5/1000, "kWh per year\n")
 
-    microCurrent = int(input("How many milliamps does your IoT microcontroller need? "))
+    microCurrent = int(input("How many milliamps per hour does your IoT microcontroller need? "))
     microVoltage = float(input("How many Volts does your IoT microcontroller need? "))    
     #microWatts = (150/1000)*3.3
-    microWatts = (microCurrent/1000)*microVoltage
-    print("Your IoT microcontroller likely needs ", microWatts, "Watts per hour or ", hours*microWatts/1000, "kWh per year\n")
+    microWatts = round((microCurrent/1000)*microVoltage,3)
+    print("Your IoT microcontroller likely needs ", round(microWatts/1000,5), "kWh or ", round(hours*microWatts/1000,2), "kWh per year\n")
+    
+    # this updates a global variable and is bad form ... to do: redo script using object oriented programming    
+    global watts 
+    #print(watts)
+    watts  = microWatts + watts
+    #print(watts)
     
 def solarPanel():
     """This function estimates the size of solar panel needed to support a microcontroller"""
     print("Anchorage Alaska receives approximately 0.7 kWh of sunlight per square meter per day in January")
+    print ("Anchorage Alaska receives approximately 6.5 hours of sunlight per day in January\n")
+    
     print("Anchorage Alaska receives approximately 5.06 kWh of sunlight per square meter per day in June")
+    print ("Anchorage Alaska receives approximately 19.2 hours of sunlight per day in June\n")    
     
 def main():    
-    yearlyHours()
+    Hours()
     microPower()
     solarPanel()
     
